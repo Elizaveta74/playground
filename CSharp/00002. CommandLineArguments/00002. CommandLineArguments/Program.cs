f2137c7b48fdc53f46e59d327c974db83fb74cb7
Rafael Osipov <rafael.osipov@outlook.com>
2023-07-08 13:42:27 +0300

    fix: Fixed command line demo project

--- CSharp/00002. CommandLineArguments/00002. CommandLineArguments/Program.cs
+++ CSharp/00002. CommandLineArguments/00002. CommandLineArguments/Program.cs
@@ -6,16 +6,22 @@
         {
             Console.WriteLine($"Total app parameters: {args.Length}");
 
-            Console.WriteLine($"First app parameter: {args[0]}");
+            // if there's at least one parameter present
+            if (args.Length > 0)
+            { 
+                Console.WriteLine($"First app parameter: {args[0]}");
+            }
 
+            // if there're at least two parameters present
             if ( args.Length > 1 ) 
             {
                 Console.WriteLine($"Second app parameter: {args[1]}");
             }
 
+            // if there are three parameters present
             if (args.Length > 2)
             {
-                Console.WriteLine($"Second app parameter: {args[2]}");
+                Console.WriteLine($"Third app parameter: {args[2]}");
             }
         }
     }
