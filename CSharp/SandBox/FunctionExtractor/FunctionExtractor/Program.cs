568c172bc10dab9b9421f0fdccf20fadfc83a0c5
Rafael Osipov <rafael.osipov@outlook.com>
2023-07-18 20:53:21 +0300

    feat - Update SandBox\FunctionExtractor project

--- CSharp/SandBox/FunctionExtractor/FunctionExtractor/Program.cs
+++ CSharp/SandBox/FunctionExtractor/FunctionExtractor/Program.cs
@@ -1,4 +1,4 @@
-﻿// Simplify the code below via extracting repeating parts to a function
+﻿// Simplify the code below via extracting repeating parts to functions
 
 Console.WriteLine("================= BEGIN OF DATA BLOCK =================");
 Console.WriteLine("Apple");
@@ -11,4 +11,34 @@ Console.WriteLine("Mango");
 Console.WriteLine("================== END OF DATA BLOCK ==================");
 Console.WriteLine("================= BEGIN OF DATA BLOCK =================");
 Console.WriteLine("Mulberry");
+Console.WriteLine("================== END OF DATA BLOCK ==================");
+Console.WriteLine("================= BEGIN OF DATA BLOCK =================");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("================== END OF DATA BLOCK ==================");
+Console.WriteLine("================= BEGIN OF DATA BLOCK =================");
+for (int i = 0; i < 13; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("================== END OF DATA BLOCK ==================");
+
+int sum = 0;
+Console.WriteLine("================= BEGIN OF DATA BLOCK =================");
+for (int i = 0; i < 15; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"sum = {sum}");
+Console.WriteLine("================== END OF DATA BLOCK ==================");
+
+sum = 0;
+Console.WriteLine("================= BEGIN OF DATA BLOCK =================");
+for (int i = 0; i < 278; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"sum = {sum}");
 Console.WriteLine("================== END OF DATA BLOCK ==================");
