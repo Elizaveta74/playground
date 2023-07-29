4b874a0a9d62b42b8035f500bbec1dfd11024317
Rafael Osipov <rafael.osipov@outlook.com>
2023-07-30 01:19:54 +0300

    feat: Update BitwiseAndShiftOperations project

--- CSharp/SandBox/BitwiseAndShiftOperations/BitwiseAndShiftOperations/Program.cs
+++ CSharp/SandBox/BitwiseAndShiftOperations/BitwiseAndShiftOperations/Program.cs
@@ -73,3 +73,56 @@ uint bitwiseNotResult = ~firstOperand;
 string bitwiseNotResultAsBinaryString = ConvertIntToBinaryString(bitwiseNotResult);
 Console.WriteLine($"firstOperand: {firstOperand},\t\tbinary representation: {firstOperandAsBinaryString}");
 Console.WriteLine($"bitwiseNotResult: {bitwiseNotResult},\tbinary representation: {bitwiseNotResultAsBinaryString}");
+
+Console.WriteLine("\n");
+
+/**
+    Bitwise operations are sometimes used to wrap a group of logical flags (bits) into a single value. 
+    The presence of bit 1 means true and the presence of bit 0 means false. And 8 logical flags (bits) 
+    can be packed into one byte in this way.
+ */
+
+// all 8 flags are set to false: 00000000
+// bit positions are counting from left to right
+byte allFlags = 0;
+
+// setting the second, third forth bits to 1
+// it is a bit-mask to be used for this task
+const byte bitMaskForSecondThirdAndForthBits = 0b00001110;
+string bitMaskForSecondThirdAndForthBitsBinaryRepresentationString = ConvertIntToBinaryString(bitMaskForSecondThirdAndForthBits);
+
+Console.WriteLine($"Setting bit mask: '{bitMaskForSecondThirdAndForthBitsBinaryRepresentationString}' ({bitMaskForSecondThirdAndForthBits}) to 00000000 (allFlags)");
+
+allFlags |= bitMaskForSecondThirdAndForthBits;
+
+string allFlagsBinaryRepresentationString = ConvertIntToBinaryString(allFlags);
+Console.WriteLine($"bit-mask has set, allFlags = {allFlags},\tbinary representation: {allFlagsBinaryRepresentationString}");
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"Clearing second bit for {allFlags},\t\tbinary representation: {allFlagsBinaryRepresentationString}");
+
+// clearing second bit in allFlags byte value
+const byte bitMaskToClearSecondBit = 0b11111101;
+
+allFlags &= bitMaskToClearSecondBit;
+
+allFlagsBinaryRepresentationString = ConvertIntToBinaryString(allFlags);
+Console.WriteLine($"Second bit cleared, allFlags = {allFlags},\tbinary representation: {allFlagsBinaryRepresentationString}");
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"Checking third bit is set or not for allFlags = {allFlags},\tbinary representation: {allFlagsBinaryRepresentationString}");
+
+// checking third bit set or not
+byte maskToCheckThirdBitSet = 0b00000100;
+
+if ((allFlags & maskToCheckThirdBitSet) != 0)
+{
+    Console.WriteLine($"allFlags has 3-rd bit set.\t\t\t\tbinary representation: {allFlagsBinaryRepresentationString}");
+}
+else
+{
+    Console.WriteLine($"allFlags has 3-rd bit NOT set\t\t\t\tbinary representation: {allFlagsBinaryRepresentationString}");
+}
+
